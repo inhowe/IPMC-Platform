01344c8782b4f182425ea36bc53770a09a0fc0be
inhowe <inhowe@qq.com>
2019-05-14 15:53:40 +0800

    auto commit

--- auto.bat
+++ auto.bat
@@ -3,16 +3,16 @@
 copy .\Software\IPMC-UCOS\Project\MDK\Output\IPMC.hex .\Software\HostComputer\doc\software\firmware\
 ::删除output文件
 call .\Software\IPMC-UCOS\keilkill.bat
-::压缩源代码
+::压缩下位机源代码
 set zip="D:\WinRAR\Rar.exe"
 %zip% a -k -r -s -m3 -ep1 .\Software\IPMC-UCOS.zip .\Software\IPMC-UCOS 
-::复制源代码
+::复制下位机源代码到上位机
 copy ..\IPMC-UCOS.zip .\Software\HostComputer\doc\software\
-::删除压缩包
+::删除下位机源代码压缩包
 del .\Software\IPMC-UCOS.zip
 
 
-::::重新压缩IPMC.zip用于上传
+::::重新压缩IPMC.zip用于发布
 del .\Software\builds\IPMC-Platform\IPMC\IPMC.zip
 %zip% a -k -r -s -m3 -ep1 .\Software\builds\IPMC-Platform\IPMC\IPMC.zip .\Software\builds\IPMC-Platform\IPMC\IPMC.exe
 del .\Software\builds\IPMC-Platform\IPMC-Platform-Install-simple.zip
